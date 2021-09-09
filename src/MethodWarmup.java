22689b92dc5a73f74bdfe3d2ef5362263d5e4eff
Orenthal Hill <ojhill98@gmail.com>
2021-09-09 18:57:52 -0500

    exercises progress

--- src/MethodWarmup.java
+++ src/MethodWarmup.java
@@ -1,12 +1,28 @@
 import java.util.Scanner;
 
-public class MethodWarmup {
-    public static void main(String[] args) {
-       getDifference(6,4);
-    }
-    public static int getDifference(int a, int b){
-        if ( a >  b) return a-b;
-        return b-a;
-
-        }
-}
+//public class MethodWarmup {
+//    public static void main(String[] args) {
+//       getDifference(6,4);
+//    }
+//    public static int getDifference(int a, int b){
+//        if ( a >  b) return a-b;
+//        return b-a;
+//public static void main(String[] args) {
+//addNums(5);
+//        System.out.println(addNums(5));
+//}
+//        public static int addNums(int num){
+//        if(num == 1) return 1;
+//        return addNums(num-1)+num;
+//        public static String getHello(){
+//        Scanner sc = new Scanner(System.in);
+//                System.out.println("Please enter the word hello:");
+//        String userResponse = sc.nextLine();
+//        if (userResponse.equalsIgnoreCase("hello")){
+//                return userResponse;
+//        }else{
+//                return getHello();
+//        }
+//
+//}
+//}
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,17 +2,75 @@ import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main(String[] args) {
-    addInt(6,6);
-    subtractInt(6,3);
+        int integer = integerInput();
+//        addInt(6, 6);
+//        subtractInt(6, 3);
+//        multiplication(4, 4);
+//        division(6, 2);
+//        modulus(20, 3);
+//        getInteger(1, 10);
+//        integerInput(1, 10);
+//        factorial(integer);
+        System.out.println(factorial(integer));
     }
-public static int addInt(int a, int b){
+
+
+    public static int addInt(int a, int b) {
         int c = a + b;
-    System.out.println(c);
+        System.out.println(c);
+        return c;
 
-}
-public static int subtractInt(int a, int b){
-        int c = a-b;
-    System.out.println(c);
+    }
 
+    public static int subtractInt(int a, int b) {
+        int c = a - b;
+        System.out.println(c);
+        return c;
+    }
+
+    public static int multiplication(int a, int b) {
+        int c = a * b;
+        System.out.println(c);
+        return c;
+    }
+
+    public static int division(int a, int b) {
+        int c = a / b;
+        System.out.println(c);
+        return c;
+    }
+
+    public static int modulus(int a, int b) {
+        int c = a % b;
+        System.out.println(c);
+        return c;
+    }
+
+    public static int getInteger(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = scanner.nextInt();
+        if (userInput < min || userInput > max) {
+            return getInteger(min, max);
+        } else {
+            return userInput;
+        }
+    }
+
+    public static int integerInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = scanner.nextInt();
+//        if (userInput > min || userInput < max) {
+        return userInput;
+//        }
+    }
+
+    public static long factorial(int x) {
+        if (x == 1) return 1;
+        return x * factorial(x - 1);
+    }
 }
-}
+
+
+
