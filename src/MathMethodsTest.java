1c8a04bf523707b8956dd7a11554ea36505d8d2c
Orenthal Hill <ojhill98@gmail.com>
2021-09-13 09:33:33 -0500

    warmup complete

---
+++ src/MathMethodsTest.java
@@ -0,0 +1,8 @@
+public class MathMethodsTest {
+    public static void main(String[] args) {
+        MyMathMethods mathMethods = new MyMathMethods();
+        System.out.println(mathMethods.multiply(5,6));
+    }
+
+
+}
---
+++ src/MyMathMethods.java
@@ -0,0 +1,8 @@
+public class MyMathMethods {
+    public int multiply(int num1, int num2){
+        int result = num1* num2;
+        return result;
+
+
+    }
+}
