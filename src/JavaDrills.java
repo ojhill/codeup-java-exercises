7c829303dcdc9bb2b09d7c518e60d8b127e92be2
Orenthal Hill <ojhill98@gmail.com>
2021-10-15 09:36:47 -0500

    warmup in progress

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -1,25 +1,35 @@
+import java.awt.*;
+import java.util.ArrayList;
+
 //work in progress
 public class JavaDrills {
-    public static String flipOuterCase(String text)
-    {
-        char[] chars = text.toCharArray();
-        for (int i = 0; i < chars.length; i++)
-        {
-            char l = chars.length-1;
-            char c = chars[i];
-            if (Character.isUpperCase(c))
-            {
-                chars[i] = Character.toLowerCase(c);
-            }
-            else if (Character.isLowerCase(c))
-            {
-                chars[i] = Character.toUpperCase(c);
-            }
-        }
-        return new String(chars);
+//    public static String flipOuterCase(String str){
+//        char[] chars = str.toCharArray();
+//
+//        for (int i = 0; i < chars.length; i++) {
+//            char c = chars[i];
+//            if (Character.isUpperCase(c)) {
+//                chars[i] = Character.toLowerCase(c);
+//            }
+//            else if (Character.isLowerCase(c))
+//            {
+//                chars[i] = Character.toUpperCase(c);
+//            }
+//        }
+//        return new String(chars);
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(flipOuterCase("Cat"));
+//
+//    }
+public static List returnTotalDifference(List<Integer> listOne, List<Integer> listTwo){
+    int sumOfOne = 0;
+    int sumOfTwo = 0;
+    for (int i=0; i< arrayOne.size(); i++){
+      if()
     }
+}
     public static void main(String[] args) {
-        System.out.println(flipOuterCase("Cat"));
 
-    }
+}
 }
