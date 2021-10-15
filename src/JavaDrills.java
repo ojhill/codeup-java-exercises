a82705f7fb84e1e35499c55764110623931dbead
Orenthal Hill <ojhill98@gmail.com>
2021-10-15 09:41:04 -0500

    warmup progress

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -22,6 +22,13 @@ public class JavaDrills {
 //        System.out.println(flipOuterCase("Cat"));
 //
 //    }
+
+//    ================================= WARM UP
+//    In `codeup-java-exercises`, in `JavaDrills`:
+//    Create a static method, returnTotalDifference, that takes in two lists of integers and returns the difference between the sum of all integers in the first list minus the sum of all integers in the second list.
+//            returnTotalDifference(Arrays.asList(10, 2, 3), Arrays.asList(1, 2, 3)); // returns 9
+//    returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7)); // returns 3
+//    returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7, 1)); // returns 2
 public static List returnTotalDifference(List<Integer> listOne, List<Integer> listTwo){
     int sumOfOne = 0;
     int sumOfTwo = 0;
