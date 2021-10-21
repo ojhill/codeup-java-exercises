3a79cb0ce8da9187a4c5985f07ac16b9c15747ab
Orenthal Hill <ojhill98@gmail.com>
2021-10-21 09:25:57 -0500

    warmup complete

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 //work in progress
 public class JavaDrills {
@@ -29,14 +30,41 @@ public class JavaDrills {
 //            returnTotalDifference(Arrays.asList(10, 2, 3), Arrays.asList(1, 2, 3)); // returns 9
 //    returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7)); // returns 3
 //    returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7, 1)); // returns 2
-public static List returnTotalDifference(List<Integer> listOne, List<Integer> listTwo){
-    int sumOfOne = 0;
-    int sumOfTwo = 0;
-    for (int i=0; i< arrayOne.size(); i++){
-      if()
-    }
-}
-    public static void main(String[] args) {
+//public static int returnTotalDifference(List<Integer> integerListOne, List<Integer> integerListTwo){
+//    int sumOfOne = 0;
+//    int sumOfTwo = 0;
+//
+//    for (int integer : integerListOne){
+//      sumOfOne += integer;
+//    }
+//    for (int integer: integerListTwo){
+//        sumOfTwo += integer;
+//    }
+//    return sumOfOne - sumOfTwo;
+//}
+//
+//
+//    public static void main(String[] args) {
+//        System.out.println(returnTotalDifference(Arrays.asList(10,2,3),Arrays.asList(1,2,3)));
+//    }
 
-}
+    public static String flipInnerCase(String str){
+        char[] chars = str.toCharArray();
+
+        for (int i = 1; i < chars.length-1; i++) {
+            char c = chars[i];
+            if (Character.isUpperCase(c)) {
+                chars[i] = Character.toLowerCase(c);
+            }
+            else if (Character.isLowerCase(c))
+            {
+                chars[i] = Character.toUpperCase(c);
+            }
+        }
+        return new String(chars);
+    }
+    public static void main(String[] args) {
+        System.out.println(flipInnerCase("TerMInAToR"));
+
+    }
 }
